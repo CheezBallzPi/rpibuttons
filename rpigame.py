7383a1409beea776d978406f8babe2c186d51bbd
Jackie Chen <cheezballz3.14@gmail.com>
2016-07-23 21:44:41 -0400

    Added many more comments to rpigame.py
    Now you can understand it!

--- rpigame.py
+++ rpigame.py
@@ -1,12 +1,25 @@
+# Raspberry Pi Button Game
+# 
+# This game makes the seven lights light up in a row very quickly. You must try to 
+# press the button when the middle light is lit up. 
+# For best use, switch the middle LED with a different color one.
+# 
+# Written by Jackie Chen
+# 
+# Why are you still reading this? Look at the actual code!
+
+# import GPIO and time
 import RPi.GPIO as GPIO
 import time
 
+# Set GPIO mode to Broadcom
 GPIO.setmode(GPIO.BCM)
 
+# Setting some constants
 sleepTime = 0.05
-
 buttonPressed = False
 
+# Setting LED Ports and other ports
 light1 = 2
 light2 = 3
 light3 = 4
@@ -17,6 +30,7 @@ light7 = 27
 
 currLight = 2
 
+# lightList will be used in a for loop later
 lightList = [light1,
              light2,
              light3,
@@ -29,9 +43,9 @@ buttonIn = 22
 buttonOut = 23
 
 # PWM pin is pin 18
-
 buzzer = 18
 
+# Setting up all pins
 GPIO.setup(light1, GPIO.OUT)
 GPIO.setup(light2, GPIO.OUT)
 GPIO.setup(light3, GPIO.OUT)
@@ -48,6 +62,7 @@ GPIO.setup(buzzer, GPIO.OUT)
 GPIO.output((light1, light2, light3, light4, light5, light6, light7, buzzer), 0)
 GPIO.output(buttonOut, 1)
 
+# buzz() and play() are used for the buzzer. Feel free to copy this if you need to use the buzzer
 
 def buzz(pitch, duration):  # create the function “buzz” and feed it the pitch and duration)
     if pitch == 0:
@@ -63,7 +78,7 @@ def buzz(pitch, duration):  # create the function “buzz” and feed it the pit
         GPIO.output(buzzer, False)  # set pin 18 to low
         time.sleep(delay)  # wait with pin 18 low
 
-
+# To edit the tunes, just change the pitch and duration list.
 def play(tune):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(buzzer, GPIO.OUT)
@@ -71,8 +86,7 @@ def play(tune):
     x = 0
     print("Playing tune ", tune)
     if tune == 1:
-        # pitches = [262, 294, 330, 349, 392, 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047]
-        # duration = 0.1
+        # A list of possible pitches? [262, 294, 330, 349, 392, 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047]
         pitches = [392, 349, 330]
         duration = [0.4, 0.4, 2]
         for p in pitches:
@@ -89,8 +103,15 @@ def play(tune):
             time.sleep(duration[x] * 0.5)
             x += 1
 
+# main
 try:
     while True:
+      
+        # prints 3, 2, 1, GO and lights light up in certain way:
+        # 3      O - - - - - O
+        # 2      - O - - - O -
+        # 1      - - O - O - -
+        # GO     - - - O - - -
         print("3")
         GPIO.output((light1, light7), 1)
         buzz(262, 0.5)
@@ -115,7 +136,10 @@ try:
             GPIO.output((light1, light2, light3, light4, light5, light6, light7, buzzer), 0)
 
             buttonPressed = False
-
+            
+            # Constantly checks if button is pressed, and then moves light. Also checks if you win or not. 
+            # This method does not use events, and is the lazy person way of doing it. It is not recommended.
+            # But I'm lazy, so derp. :b
             for light in lightList:
                 GPIO.output(light, 1)
                 currLight = light
@@ -144,5 +168,6 @@ try:
                 break
         GPIO.output((light1, light2, light3, light4, light5, light6, light7, buzzer), 0)
     GPIO.output((light1, light2, light3, light4, light5, light6, light7, buttonOut, buzzer), 0)
-except KeyboardInterrupt:
+except:
+    # If it all breaks, turn all pins off.
     GPIO.output((light1, light2, light3, light4, light5, light6, light7, buttonOut, buzzer), 0)
